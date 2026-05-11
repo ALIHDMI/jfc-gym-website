d0f2ff3b2731602bd4105c7d8da19decfbd2b7d0
ALI <91565870+ALIHDMI@users.noreply.github.com>
2026-05-11 14:18:38 +0600

    Add basePath and assetPrefix for deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: "/jfc-gym-website",
+  assetPrefix: "/jfc-gym-website/",
   distDir: "output",
   images: {
     unoptimized: true,
